717d409cbcaa6656f2213b48cbfb8f06a99ed076
Christoph Caks <ccaks@bestsolution.at>
2020-10-19 12:59:35 +0200

    update driftcpp

--- org.eclipse.fx.drift.samples.cpp.native/src/main/headers/driftcpp.h
+++ org.eclipse.fx.drift.samples.cpp.native/src/main/headers/driftcpp.h
@@ -1,3 +1,14 @@
+/* ******************************************************************************
+ * Copyright (c) 2019, 2020 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License 2.0 
+ * which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Christoph Caks <ccaks@bestsolution.at> - initial API and implementation
+ * ******************************************************************************/
 #ifndef _DRIFTCPP_H_
 #define _DRIFTCPP_H_
 
@@ -15,6 +26,11 @@ namespace driftfx {
     struct Vec2i {
         int x;
         int y;
+    };
+
+    struct Vec2d {
+        double x;
+        double y;
     };
     
     class TransferType {
@@ -43,12 +59,35 @@ namespace driftfx {
         virtual ~Swapchain() = 0;
     };
     
-    
-    
     class Renderer {
     public:
+        /**
+         * creates a new Swapchain.
+         * <p>must be called from the rendering thread with a current OpenGL context</p>
+         * @return a new swapchain
+         */
         virtual Swapchain* createSwapchain(SwapchainConfig config) = 0;
+        /**
+         * returns the current size of the associated driftfx surface.
+         * <p>this is the premultiplied size for the texture (logical size * screen scale * user scale)</p>
+         * @return the size
+         */
         virtual Vec2i getSize() = 0;
+        /**
+         * the current logical size of the associated driftfx surface.
+         * @return the logical size
+         */
+        virtual Vec2d getLogicalSize() = 0;
+        /**
+         * the current screen scale of the associated driftfx surface as reported by javafx.
+         * @return the screen scale.
+         */
+        virtual Vec2d getScreenScale() = 0;
+         /**
+         * the current user scale of the associated driftfx surface.
+         * @return the user scale
+         */
+        virtual Vec2d getUserScale() = 0;
         virtual ~Renderer() = 0;
     };
     
@@ -72,7 +111,6 @@ namespace driftfx {
         static TransferType* IOSurface;
         static TransferType* NVDXInterop;
     };
-    
     
     // initializes drift cpp
     // must be called before any other method
